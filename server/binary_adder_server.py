07aa30dbdcba056534f989981ce500bf89c748b8
eric <ericc3141@users.noreply.github.com>
2018-03-04 01:44:22 -0800

    Add rooms to binary adder server

--- server/binary_adder_server.py
+++ server/binary_adder_server.py
@@ -1,26 +1,67 @@
 import asyncio
 import websockets
 import json
+import random
 
-a = [0, 0, 1]
-b = [1, 0, 1]
-c = [0, 0, 0]
-s0 = [1, 1, 0]
-s = [0, 0, 0]
+class Room:
+    def __init__(self):
+        self.size = 0
+        self.generateNums()
+        self.connected = []
+
+    def generateNums(self):
+        carry = 0
+        self.a = []
+        self.b = []
+        self.c = []
+        self.s = []
+        self.s0 = []
+        for i in range(0, self.size):
+            self.a.append(random.randint(0,1))
+            self.b.append(random.randint(0,1))
+            self.s.append(0)
+            self.c.append(0)
+            n = self.a[i] + self.b[i] + carry
+            self.s0.append((0, 1)[n == 1 or n == 3])
+            carry = (0, 1)[n > 2]
+
+    def add(self, ws):
+        self.connected.append(ws)
+        self.size += 1
+        self.generateNums()
+
+    def rm(self, ws):
+        self.connected.remove(ws)
+        self.size -= 1
+        self.generateNums()
+
+rooms = {}
+
 connected = []
-bits = []
 async def handler(websocket, path):
     global connected
     # Register.
     clientId = len(connected)
+    room = ""
     connected.append(websocket)
     try:
         # Implement logic here.
         while True:
             mssg = await websocket.recv()
             obj = json.loads(mssg)
-            s[clientId] = obj["s"]
-            c[clientId] = obj["c"]
+            if ("room" in obj):
+                room = obj["room"]
+                if (not(room in rooms)):
+                    rooms[room] = Room()
+                clientId = len(rooms[room].connected)
+                rooms[room].add(websocket)
+                continue
+
+            if (room == ""):
+                continue
+
+            rooms[room].s[clientId] = obj["s"]
+            rooms[room].c[clientId] = obj["c"]
             #await asyncio.wait([ws.send("Hello!") for ws in connected])
     except Exception as e:
         # Unregister.
@@ -29,12 +70,13 @@ async def handler(websocket, path):
 
 async def update():
     while True:
-        print("Update {}".format(len(connected)))
-        print(s)
-        for i in range(0, len(connected)):
-            carry = (c[i-1], 0)[i==0]
-            mssg = json.dumps({ "a":a[i], "b":b[i], "c":carry })
-            await connected[i].send(mssg)
+        for r in rooms:
+            rm = rooms[r]
+            print("{} {} {}".format(r, rm.c, rm.s))
+            for i in range(0, len(rm.connected)):
+                carry = (rm.c[i-1], 0)[i==0]
+                mssg = json.dumps({ "a":rm.a[i], "b":rm.b[i], "c":carry })
+                await rm.connected[i].send(mssg)
         await asyncio.sleep(0.5)
 
 start_server = websockets.serve(handler, 'localhost', 8767)
