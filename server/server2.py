57ec39e2904d310453114bc86a4d329b0afdbdfa
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-03-03 17:01:26 -0800

    new server approach

---
+++ server/server2.py
@@ -0,0 +1,29 @@
+import asyncio
+import signal
+import websockets
+
+from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWebSocketServer
+
+clients = set()
+class Server(WebSocket):
+   def handleMessage(self):
+      for client in clients:
+         if client != self:
+            client.sendMessage(self.data)
+
+   def handleConnected(self):
+      clients.add(self)
+
+   def handleClose(self):
+      clients.remove(self)
+
+
+server = SimpleWebSocketServer("localhost", 8765, Server)
+
+def close_sig_handler(signal, frame):
+    server.close()
+    sys.exit()
+
+signal.signal(signal.SIGINT, close_sig_handler)
+
+server.serveforever()
